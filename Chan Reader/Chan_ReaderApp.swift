3e6569f3f5099ee94cac78d16f4fc2625d1c1793
Aman <amanbind007@gmail.com>
2024-11-06 22:32:49 +0530

    Initial Commit

---
+++ Chan Reader/Chan_ReaderApp.swift
@@ -0,0 +1,32 @@
+//
+//  Chan_ReaderApp.swift
+//  Chan Reader
+//
+//  Created by Aman Bind on 06/11/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Chan_ReaderApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
