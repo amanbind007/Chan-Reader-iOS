e2d5a33df4f882b093bc9b51aeff39fa8ea19a28
Aman Bind <75306571+amanbind007@users.noreply.github.com>
2024-11-15 22:53:16 +0530

    Update Catalog model
    fixed optional values

--- Chan Reader/Models/Catalog.swift
+++ Chan Reader/Models/Catalog.swift
@@ -10,42 +10,42 @@ import Foundation
 struct Catalog: Codable {
     let no: Int
     let resto: Int
-    let sticky: Int
-    let closed: Int
+    let sticky: Int?
+    let closed: Int?
     let now: String
     let time: Int
     let name: String
-    let trip: String
-    let id: String
-    let capcode: String
-    let country: String
-    let country_name: String
-    let sub: String
-    let com: String
-    let tim: Int
-    let filename: String
-    let ext: String
-    let fsize: Int
-    let md5: String
-    let w: Int
-    let h: Int
-    let tn_w: Int
-    let tn_h: Int
-    let filedeleted: Int
-    let spoiler: Int
-    let custom_spoiler: Int
-    let omitted_posts: Int
-    let omitted_images: Int
-    let replies: Int
-    let images: Int
-    let bumplimit: Int
-    let imagelimit: Int
-    let last_modified: Int
-    let tag: String
-    let semantic_url: String
-    let since4pass: Int
-    let unique_ips: Int
-    let m_img: Int
+    let trip: String?
+    let id: String?
+    let capcode: String?
+    let country: String?
+    let country_name: String?
+    let sub: String?
+    let com: String?
+    let tim: Int?
+    let filename: String?
+    let ext: String?
+    let fsize: Int?
+    let md5: String?
+    let w: Int?
+    let h: Int?
+    let tn_w: Int?
+    let tn_h: Int?
+    let filedeleted: Int?
+    let spoiler: Int?
+    let custom_spoiler: Int?
+    let omitted_posts: Int?
+    let omitted_images: Int?
+    let replies: Int?
+    let images: Int?
+    let bumplimit: Int?
+    let imagelimit: Int?
+    let last_modified: Int?
+    let tag: String?
+    let semantic_url: String?
+    let since4pass: Int?
+    let unique_ips: Int?
+    let m_img: Int?
     let last_replies: [LastReply]
 }
 
